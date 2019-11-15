1019d95a2b006bc4b1c6cf7c5af7d3d39d6914c2
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 16:41:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,6 @@ def get_japanese_emoticon(file_path = "./lib/emoticons.yml", enlgish_emoticon)
   library = load_library(file_path)
   if library["get_emoticon"].include?(english_emoticon)
      library["get_emoticon"][english_emoticon]
-   else 
-     "Emoticon not found"
   end 
 end
 
@@ -24,7 +22,5 @@ def get_english_meaning(file_path = "./lib/emoticons.yml", japanese_emoticon)
   library = load_library(file_path)
   if library["get_meaning"].include?(japanese_emoticon)
     library["get_meaning"][japanese_emoticon]
-  else 
-    "Emoticon not found"
   end 
 end
