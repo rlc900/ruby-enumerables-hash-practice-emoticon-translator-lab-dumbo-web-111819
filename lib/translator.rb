91c7185ebe581adb36752791feac7e7787b952f7
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 16:25:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,14 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+
+def load_library(file_path)
+ library = YAML.load_file(file_path)
+ result = {"get_meaning" => {}, "get_emoticon" => {}}
+ library.each do |meaning, emoticon|
+   result["get_meaning"][emoticon[1]] = meaning 
+   result["get_emoticon"][emoticon[0]] - emoticon[1]
+ end 
+ result
 end
 
 def get_japanese_emoticon
